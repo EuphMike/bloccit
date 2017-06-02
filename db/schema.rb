dae4667ca5bc4b197439d8dfaadd8faff4e147b8
Mike Waddell <waddellmusic@gmail.com>
2017-06-02 16:29:03 +0000

    updatedCheckpoint14-Assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602134815) do
+ActiveRecord::Schema.define(version: 20170602140234) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170602134815) do
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "resolve"
+    t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
