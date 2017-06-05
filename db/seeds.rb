964b17d5f8fc51a861e62b1007086b248cc4aa83
Mike Waddell <waddellmusic@gmail.com>
2017-06-05 20:12:08 +0000

    completedcheckpoint16-Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,26 @@
  require 'random_data'
 
- # Create Posts
  50.times do
- # #1
+     
    Post.create!(
- # #2
+
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
  
- # Create Comments
- # #3
+
  100.times do
    Comment.create!(
- # #4
+
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
+ 
+ Post.find_or_create_by(title: "Test", body: "Test,Test,Test")
+ Comment.create_with(title: "Test").find_or_create_by(body: "testy test test")
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
