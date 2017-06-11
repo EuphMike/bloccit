647d630da80270cb0691e6d88827220197b9d0b5
Mike Waddell <waddellmusic@gmail.com>
2017-06-11 20:51:34 +0000

    updated migrate, still errors

---
+++ db/migrate/20170611203826_add_topic_id_to_sponsored_posts.rb
@@ -0,0 +1,5 @@
+class AddTopicIdToSponsoredPosts < ActiveRecord::Migration
+  def change
+    add_reference :sponsored_posts, :topic, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610164034) do
+ActiveRecord::Schema.define(version: 20170611203826) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -55,7 +55,10 @@ ActiveRecord::Schema.define(version: 20170610164034) do
     t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
+
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
