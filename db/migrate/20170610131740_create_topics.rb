4572ce7a6d26b3e133d7f2c398714eaed4033784
Mike Waddell <waddellmusic@gmail.com>
2017-06-12 13:14:28 +0000

    fixed post bugs

--- db/migrate/20170610131740_create_topics.rb
+++ db/migrate/20170610131740_create_topics.rb
@@ -1,11 +1,11 @@
-class CreateTopics < ActiveRecord::Migration
-  def change
-    create_table :topics do |t|
-      t.string :name
-      t.boolean :public, default: true 
-      t.text :description
+ class CreateTopics < ActiveRecord::Migration
+   def change
+     create_table :topics do |t|
+       t.string :name
+       t.boolean :public, default: true
+       t.text :description
 
-      t.timestamps
-    end
-  end
-end
+       t.timestamps
+     end
+   end
+ end
