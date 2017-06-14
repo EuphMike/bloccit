2eac36c85ee6112e59e3ec723420d30697e7db78
Mike Waddell <waddellmusic@gmail.com>
2017-06-14 14:43:44 +0000

    completedAssignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,17 @@
    def new
      @user = User.new
    end
+   
+    def confirm
 
-   def create
+     @user = User.new
+     @user.name = params[:user][:name]
+     @user.email = params[:user][:email]
+     @user.password = params[:user][:password]
+     @user.password_confirmation = params[:user][:password_confirmation]
+    end
+ 
+    def create
 
      @user = User.new
      @user.name = params[:user][:name]
